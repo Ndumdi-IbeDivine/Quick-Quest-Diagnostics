6c2310634fdbb29e4c7f3471fd68d9b2de629c3c
Joshua-Nweze <joshuanweze270@gmail.com>
2025-04-07 01:18:05 +0100

    ux: improve sm nav ux

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <nav class="grid grid-cols-2 lg:pe-8 border border-1 border-white items-center fixed website-margins left-0 right-0 top-5 text-white navbar" ref="navbar">
+        <nav class="grid grid-cols-2 md:pe-4 lg:pe-8 border border-1 border-white items-center fixed website-margins left-0 right-0 top-5 text-white navbar" ref="navbar">
             <div>
                 <NuxtLink to="/">
                     <img src="/assets/images/qqd-logo.png" class="size-20" alt="">
@@ -18,27 +18,26 @@
                     <i v-else class="fa-solid fa-xmark"></i>
                 </button>
             </div> -->
-
-            <div class="lg:hidden flex justify-end pe-3">
-                <button data-collapse-toggle="navbar-default"  type="button" class="flex justify-end items-center border border-1 rounded p-2  h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <div class="md:hidden flex justify-end pe-3">
+                <button @click="showNavDropdown = !showNavDropdown"   type="button" class="flex justify-end items-center border border-1 rounded p-2 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
                     <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <svg v-if="!showNavDropdown" class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
-                    <!-- <i v-if="!showNavDropdown" class="fa-solid fa-bars"></i>
-                    <i v-else class="fa-solid fa-xmark"></i> -->
+                    <!-- <i v-if="!showNavDropdown" class="fa-solid fa-bars"></i> -->
+                    <i v-else class="fa-solid fa-xmark fa-1x w-5 h-5"></i>
                 </button>
             </div>
-
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                <div class="hidden lg:flex gap-16 text-slate-200 justify-end">
+            <!-- {{ showNavDropdown }} -->
+            <div class="w-full" id="navbar-default">
+                <div class="hidden md:flex gap-16 text-slate-200 justify-end">
                     <RouterLink v-for="navItem in navItems" :key="navItem.name" :to="navItem.link" active-class="active">
                         {{ navItem.name }}
                     </RouterLink>
                 </div>
 
-                <div class="lg:hidden flex flex-col gap-5  bg-[#09041C] text-white p-5 rounded-lg shadow-lg">
-                    <RouterLink v-for="navItem in navItems" :key="navItem.name" :to="navItem.link" active-class="active">
+                <div v-if="showNavDropdown" class="md:hidden mb-2 flex flex-col gap-5 transition delay-150 duration-300 ease-in-out bg-[#09041C] text-white p-5 rounded-lg shadow-lg">
+                    <RouterLink v-for="navItem in navItems" @click="showNavDropdown = false" :key="navItem.name" :to="navItem.link" active-class="active">
                         {{ navItem.name }}  
                     </RouterLink>
                 </div>
