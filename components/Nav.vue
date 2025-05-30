b942721d450acb7a7ce21b80a24df2a9570a72d1
Joshua-Nweze <joshuanweze270@gmail.com>
2025-05-30 17:28:35 +0100

    refactor: clean up Nav component template and improve dropdown handling

--- components/Nav.vue
+++ components/Nav.vue
@@ -6,53 +6,31 @@
                     <img src="/assets/images/qqd-logo.png" class="size-20" alt="">
                 </NuxtLink>
             </div>
-
-            <!-- <div class="hidden lg:flex gap-16 text-slate-200 justify-end">
-                <RouterLink v-for="navItem in navItems" :key="navItem.name" :to="navItem.link" active-class="active">
-                    {{ navItem.name }}
-                </RouterLink>
-            </div> -->
-            <!-- <div class="lg:hidden flex justify-end pe-3">
-                <button @click="showNavDropdown = !showNavDropdown" class="px-3 py-2 border border-1 rounded">
-                    <i v-if="!showNavDropdown" class="fa-solid fa-bars"></i>
-                    <i v-else class="fa-solid fa-xmark"></i>
-                </button>
-            </div> -->
             <div class="md:hidden flex justify-end pe-3">
                 <button @click="showNavDropdown = !showNavDropdown"   type="button" class="flex justify-end items-center border border-1 rounded p-2 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
                     <span class="sr-only">Open main menu</span>
-                    <svg v-if="!showNavDropdown" class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
-                    <!-- <i v-if="!showNavDropdown" class="fa-solid fa-bars"></i> -->
-                    <i v-else class="fa-solid fa-xmark fa-1x w-5 h-5"></i>
+                    <!-- <i v-else class="fa-solid fa-xmark fa-1x w-5 h-5"></i> -->
                 </button>
             </div>
             <!-- {{ showNavDropdown }} -->
-            <div class="w-full" id="navbar-default">
-                <div class="hidden md:flex gap-16 text-slate-200 justify-end">
-                    <RouterLink v-for="navItem in navItems" :key="navItem.name" :to="navItem.link" active-class="active" class="text-lg hover:underline">
-                        {{ navItem.name }}
-                    </RouterLink>
+            <ClientOnly>
+                <div class="w-full" id="navbar-default">
+                    <div class="hidden md:flex gap-16 text-slate-200 justify-end">
+                        <RouterLink v-for="navItem in navItems" :key="navItem.name" :to="navItem.link" active-class="active" class="text-lg hover:underline">
+                            {{ navItem.name }}
+                        </RouterLink>
+                    </div>
+    
+                    <div v-show="showNavDropdown" class="md:hidden mb-2 flex flex-col gap-5 transition delay-150 duration-300 ease-in-out bg-[#09041C] text-white p-5 rounded-lg shadow-lg">
+                        <RouterLink v-for="navItem in navItems" @click="showNavDropdown = false" :key="navItem.name" :to="navItem.link" active-class="active">
+                            {{ navItem.name }}  
+                        </RouterLink>
+                    </div>
                 </div>
-
-                <div v-show="showNavDropdown" class="md:hidden mb-2 flex flex-col gap-5 transition delay-150 duration-300 ease-in-out bg-[#09041C] text-white p-5 rounded-lg shadow-lg">
-                    <RouterLink v-for="navItem in navItems" @click="showNavDropdown = false" :key="navItem.name" :to="navItem.link" active-class="active">
-                        {{ navItem.name }}  
-                    </RouterLink>
-                </div>
-            </div>
-
-             <!-- <div v-if="!showNavDropdown">
-                this is cra cra
-             </div>
-             {{ showNavDropdown }}
-            <div class="lg:hidden flex flex-col gap-5  bg-[#09041C] text-white p-5 rounded-lg shadow-lg">
-                motor bike
-                <RouterLink v-for="navItem in navItems" :key="navItem.name" :to="navItem.link" active-class="active">
-                    {{ navItem.name }}  
-                </RouterLink>
-            </div> -->
+            </ClientOnly>
         </nav>
     </div>
 </template>
